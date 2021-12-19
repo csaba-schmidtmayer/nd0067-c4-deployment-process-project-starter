7a084661874afe4da751ae990083e1206669a875
Csaba Schmidtmayer <cs.schmidtmayer@gmail.com>
2021-12-19 22:07:21 +0100

    feat: Add production API endpoint

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost: "http://udagramapi-env.eba-hy3vmqia.us-east-1.elasticbeanstalk.com/",
 };
 
 /*
